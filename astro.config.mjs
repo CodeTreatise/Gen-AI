1e4655f4852f844d10a7717f1f82659134a228a9
Shivprasad <murgude.shivpa@gmail.com>
2026-01-28 16:27:33 +0530

    Make Getting Started section collapsible

--- astro.config.mjs
+++ astro.config.mjs
@@ -62,6 +62,7 @@ export default defineConfig({
       sidebar: [
         {
           label: 'Getting Started',
+          collapsed: false,
           items: [
             { label: 'Introduction', slug: 'introduction' },
           ],
